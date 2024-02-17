b65117e94d0d6e196d903fa2b636a8e003b5a85e
Lukasz <dlugozima1993@gmail.com>
2024-02-17 19:04:23 +0100

    AddingNewFiles

--- DataEngineeringProject4/dags/WebScrapingPythonConversionRateChangePercentage.py
+++ DataEngineeringProject4/dags/WebScrapingPythonConversionRateChangePercentage.py
@@ -4,6 +4,8 @@ import json
 import time
 import pandas as pd
 import logging
+import io  
+from minio import Minio 
 
 def MakingRequest(url: str) -> str:
     URL = 'https://www.x-rates.com/table/?from=USD&amount=1'
@@ -69,7 +71,7 @@ def CreateDataFrameFromJSONPercentageChange(json):
     dfPercentChangeIntheLast24Hours.reset_index(inplace=True)
     dfPercentChangeIntheLast24Hours = dfPercentChangeIntheLast24Hours.set_axis(['ConversionCurrency', 'PercentChange'], axis=1)
     today_ts = pd.Timestamp.today()
-    dfPercentChangeIntheLast24Hours['LOADINGDATA'] = today_ts
+#    dfPercentChangeIntheLast24Hours['LOADINGDATA'] = today_ts
     dfPercentChangeIntheLast24Hours     
 
     return dfPercentChangeIntheLast24Hours       
@@ -90,6 +92,48 @@ def finalExecutionOfRateChangePercentage():
     print(f"'\033[96m' DataFrame of of the Percetange of the Rate Change: \n {df}")
     SaveToCSVFile(df)
     
+def UploadDataMinioExchangeRatePercentageChange():
+    timestr = time.strftime("%d-%m-%Y")
+    URL = 'https://www.x-rates.com/table/?from=USD&amount=1'
+    MakingRequest(URL)
+    RateChangePercentage = DictionaryConversionRateChangePercentage()
+    JSONConversionRateChangePercentage = ConvertingDictToJSON(RateChangePercentage)
+    df = CreateDataFrameFromJSONPercentageChange(JSONConversionRateChangePercentage)
+    try: 
+        client = Minio(endpoint='host.docker.internal:9000', 
+                access_key='i9w6Gi54Aeu6QuFyQm2L',  
+                secret_key='5WbJFrT6KESNtJ0toxEAx9dzrAXYT3vaO3GEdOEW', 
+                    secure=False)  
+        if not client.bucket_exists("conversionrateexchange"):
+            client.make_bucket("conversionrateexchange")
+            print("\033[92m Bucket conversionrateexchange created successfully.")
+              
+    except Exception as err:
+        print(f"Error occurred: {err}")
+        
+    ListOfAllAccessibleBuckets = print(f"\033[94m Total buckets:  {len(client.list_buckets())}"),  
+    csv_bytes = df.to_csv().encode('utf-8')
+    csv_buffer = io.BytesIO(csv_bytes)
+    try: 
+        UploadCSVFileToObject = client.put_object("conversionrateexchange", 
+                        'RawDataPercentChangeIntheLast24Hours.csv',  
+                        data=csv_buffer, 
+                        length=len(csv_bytes), 
+                        content_type='application/csv')
+        
+        objects = client.list_objects("conversionrateexchange")
+        for obj in objects:
+            ListObjectInformationOfBucket = print(f"\033[95m List of Bucket Object {obj}")
+        return  UploadCSVFileToObject , ListObjectInformationOfBucket, ListOfAllAccessibleBuckets
+    except Exception as err:
+        print(f"Error occurred: {err}")
+
+
+    
 if __name__ == "__main__":
     finalExecutionOfRateChangePercentage()
+    print("\033[92m Data Conversion Rate Change Percentage was successfully saved as a CSV and JSON files")
+    UploadDataMinioExchangeRatePercentageChange()
+    print("\033[92m Data Conversion Rate Change Percentage was successfully loaded to an object in a bucket")
+
     
--- DataEngineeringProject4/dags/WebScrapingPythonExchangeCurrency.py
+++ DataEngineeringProject4/dags/WebScrapingPythonExchangeCurrency.py
@@ -4,6 +4,8 @@ import json
 import time
 import pandas as pd
 import logging
+import io  
+from minio import Minio 
 
 def MakingRequest(url: str) -> str:
     URL = 'https://www.x-rates.com/table/?from=USD&amount=1'
@@ -97,7 +99,7 @@ def CreateDataFrameFromJSON(json):
     USDollar =pd.DataFrame([{'Currency':'US Dollar', 'Rate':'1.0'}]) 
     dfUSDollarExchangeRates = pd.concat([USDollar, dfUSDollarExchangeRates], ignore_index=True)
     today_ts = pd.Timestamp.today()
-    dfUSDollarExchangeRates['LOADINGDATA'] = today_ts
+#    dfUSDollarExchangeRates['LOADINGDATA'] = today_ts
     
     return dfUSDollarExchangeRates
         
@@ -130,7 +132,53 @@ def finalExecutionOfExchangeRate():
     PrintingDataFrameOfExchangeRate()
     SaveToCSVFile(dfUSDollarExchangeRates)
 
+def UploadDataMinioExchangeCurrency():
+    timestr = time.strftime("%d-%m-%Y")
+    URL = 'https://www.x-rates.com/table/?from=USD&amount=1'
+    MakingRequest(URL)
+    DictionaryDataCurrencyValues = DictionaryOfExchangeRate()
+    JSONCurrencyValues = ConvertingDictToJSON(DictionaryDataCurrencyValues)
+    dfUSDollarExchangeRates = CreateDataFrameFromJSON(JSONCurrencyValues)
+    df =  dfUSDollarExchangeRates
+    try: 
+        client = Minio(endpoint='host.docker.internal:9000', 
+                access_key='i9w6Gi54Aeu6QuFyQm2L',  
+                secret_key='5WbJFrT6KESNtJ0toxEAx9dzrAXYT3vaO3GEdOEW', 
+                    secure=False)  
+        if not client.bucket_exists("usdollarexchangerates"):
+            client.make_bucket("usdollarexchangerates")
+            print("\033[92m Bucket usdollarexchangerates created successfully.")
+              
+    except Exception as err:
+        print(f"Error occurred: {err}")
+        
+    ListOfAllAccessibleBuckets = print(f"\033[94m Total buckets:  {len(client.list_buckets())}"),  
+    csv_bytes = df.to_csv().encode('utf-8')
+    csv_buffer = io.BytesIO(csv_bytes)
+    try: 
+        UploadCSVFileToObject = client.put_object("usdollarexchangerates", 
+                        'RawDataUSDollarExchangeRates.csv',  
+                        data=csv_buffer, 
+                        length=len(csv_bytes), 
+                        content_type='application/csv')
+        
+        objects = client.list_objects("conversionrateexchange")
+        for obj in objects:
+            ListObjectInformationOfBucket = print(f"\033[95m List of Bucket Object {obj}")
+        return  UploadCSVFileToObject , ListObjectInformationOfBucket, ListOfAllAccessibleBuckets
+    except Exception as err:
+        print(f"Error occurred: {err}")
+
+
     
 if __name__ == "__main__":
     finalExecutionOfExchangeRate()
+    print("\033[92m Data ExchangeCurrency was successfully saved as a CSV and JSON files")
+    UploadDataMinioExchangeCurrency()
+    print("\033[92m Data ExchangeCurrency was successfully loaded to an object in a bucket")
+
+    
+    
+
+    
     
